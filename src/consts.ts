8a12310a95c145ea8e0da6f73b7f388eb0e9040d
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-04-15 23:07:12 +0200

    feat: article landings and subcollections

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,62 @@
 
 export const SITE_TITLE = 'Marco Petri';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const COLLECTIONS = {
+    'blog': {
+        'heroTitle': 'Tech Blog',
+        'heroText': 'XXXXXXX',
+        'subcollections': {
+            'research': {
+                'heroTitle': 'Research',
+                'heroText': 'XXXXXXX',
+            },
+            'software_engineering': {
+                'heroTitle': 'Software Engineering',
+                'heroText': 'XXXXXXX',
+            },
+            'machine_learning': {
+                'heroTitle': 'Machine Learning',
+                'heroText': 'XXXXXXX',
+            },
+            'devops': {
+                'heroTitle': 'DevOps',
+                'heroText': 'XXXXXXX',
+            },
+            'projects': {
+                'heroTitle': 'Projects',
+                'heroText': 'XXXXXXX',
+            },
+            'productivity': {
+                'heroTitle': 'Productivity',
+                'heroText': 'XXXXXXX',
+            },
+        }
+    },
+    'opinions': {
+        'heroTitle': 'Opinions',
+        'heroText': 'XXXXXXX',
+        'subcollections': {
+            'life': {
+                'heroTitle': 'Life',
+                'heroText': 'XXXXXXX',
+            },
+            'economy': {
+                'heroTitle': 'Economy',
+                'heroText': 'XXXXXXX',
+            },
+            'politics': {
+                'heroTitle': 'Politics',
+                'heroText': 'XXXXXXX',
+            },
+            'society': {
+                'heroTitle': 'Society',
+                'heroText': 'XXXXXXX',
+            },
+            'religion': {
+                'heroTitle': 'Religion',
+                'heroText': 'XXXXXXX',
+            },
+        }
+    }
+}
