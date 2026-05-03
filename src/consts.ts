24605be8c54f8e52f2ebd41a65225792597bcda3
Marco Petri <61703580+marcopetri98@users.noreply.github.com>
2026-05-04 01:03:38 +0200

    fix: blog and opinions descriptions

--- src/consts.ts
+++ src/consts.ts
@@ -2,62 +2,54 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Marco Petri';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = "This is Marco Petri's personal website, where he shares tech posts and general opinions..";
 
 export const COLLECTIONS = {
     'blog': {
         'heroTitle': 'Tech Blog',
-        'heroText': 'XXXXXXX',
+        'heroText': "This is where I write articles about technology and computer science. You will find various categories of topics that I enjoy and would like to write about. These articles won't be too difficult to understand; however, for the sake of your mental sanity, I will list any necessary prerequisites in the first few lines of each post.",
         'subcollections': {
             'research': {
                 'heroTitle': 'Research',
-                'heroText': 'XXXXXXX',
+                'heroText': "This is where I write about everything involving research. I might write about ethics, burdens, joys, and more. This is not intended to be a section where I showcase my work; rather, it is meant as a collection of reflections on the current research process in academia.",
             },
             'software_engineering': {
                 'heroTitle': 'Software Engineering',
-                'heroText': 'XXXXXXX',
+                'heroText': "This is where I write about software engineering best practices, use cases, tools, and many other topics related to software development and design. At the moment, I don't plan on covering the basics of programming or anything like that, but feel free to drop me an email if you're interested.",
             },
             'machine_learning': {
                 'heroTitle': 'Machine Learning',
-                'heroText': 'XXXXXXX',
+                'heroText': "Welcome to my favorite world! This is where I write about artificial intelligence from every perspective. You might find a deep dive into a scientific paper I've read, an explanation of an algorithm, or even thoughts on what is currently happening in the AI research community.",
             },
             'devops': {
                 'heroTitle': 'DevOps',
-                'heroText': 'XXXXXXX',
+                'heroText': "Oooops. Did you forget to set up your CI/CD? Did you forget how to deploy your website? You shouldn't have, because you should be using DevOps best practices, and that is exactly what I write about in this small section of my website. This definitely isn't the sexiest topic, but it is useful, likely much more than what you think.",
             },
             'projects': {
                 'heroTitle': 'Projects',
-                'heroText': 'XXXXXXX',
+                'heroText': "Let's ship it. On this section, I write about projects from the ground up. I talk about project management, creating deliverables, managing technical debt, and more. Let's be honest: nobody likes it, but everybody needs it. Maybe you can teach me something, or hopefully, I can help you learn something.",
             },
             'productivity': {
                 'heroTitle': 'Productivity',
-                'heroText': 'XXXXXXX',
+                'heroText': "No, I'm not some guru selling a thousand-dollar course. Here, you'll find the methods I use to boost my productivity, and the ones you should avoid so you don't kill yours. Let's be honest: someone out there is bettering themselves right now. Do you really want to just sit back and watch the world progress while you drink your beer? There's nothing wrong with that, but at this rate, inflation will turn it into half a beer in a few years. Let's keep it a full pint.",
             },
         }
     },
     'opinions': {
         'heroTitle': 'Opinions',
-        'heroText': 'XXXXXXX',
+        'heroText': "Wait, people have opinions? Yes, they do, and welcome to democracy, my friend. While you won't be able to be a hater who just sends random hate through comments here, you might be interested in hearing others' opinions on recent events. Stay tuned.",
         'subcollections': {
             'life': {
                 'heroTitle': 'Life',
-                'heroText': 'XXXXXXX',
+                'heroText': "It's a pretty broad term, isn't it? Yes, but I wanted a general category to help classify some overall perspectives on life. Here, we delve deep into visions and ideas; consider this a warning that you might find something you disagree with. Read at your own risk of stepping outside your comfort zone.",
             },
             'economy': {
                 'heroTitle': 'Economy',
-                'heroText': 'XXXXXXX',
+                'heroText': "Money is something everyone loves and chases. Here, we talk about money, taxes, national and international policies, stock markets, bonds, and more. These topics interest me, and if you clicked on this category, I bet they interest you too.",
             },
-            'politics': {
-                'heroTitle': 'Politics',
-                'heroText': 'XXXXXXX',
-            },
-            'society': {
-                'heroTitle': 'Society',
-                'heroText': 'XXXXXXX',
-            },
-            'religion': {
-                'heroTitle': 'Religion',
-                'heroText': 'XXXXXXX',
+            'politics-society': {
+                'heroTitle': 'Politics and society',
+                'heroText': "Left or right? I know you're wondering about this. But let me tell you, the world isn't just black or white; there are plenty of shades of gray in between. Moreover, there is a huge difference between economic and societal axes. You will agree on some things, just as you'll disagree on others. Just try to be polite.",
             },
         }
     }
